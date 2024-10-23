eb6eab03a5b047c8c4ca559742a0f4aec2a688db
Syed-AliRaza005 <162878270+Syed-AliRaza005@users.noreply.github.com>
2024-10-23 18:35:46 +0500

    Add files via upload

---
+++ my app/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className=" bg-red-400">
+      <div className="container mx-auto text-center">
+        <p className="text-lg font-semibold ">
+          © 2024 Ali Raza. All rights reserved.
+        </p>
+        <div className="mt-3">
+       
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ my app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className=" font-serif font-black text-4xl md:text-5xl text-center bg-blue-400">
+      <h1>Tailwind css Assignment Page</h1>
+    </header>
+  );
+};
+
+export default Header;
